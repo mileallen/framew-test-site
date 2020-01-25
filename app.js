dcdee8c7ab1b4a72ecd258a9ad32e16e118c4ed1
mileallen <31962034+mileallen@users.noreply.github.com>
2020-01-25 23:28:04 +0530

    Add files via upload
    changed fireb to own datab

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var emailRE = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(
 
 // Setup Firebase
 var config = {
-  apiKey: "AIzaSyAi_yuJciPXLFr_PYPeU3eTvtXf8jbJ8zw",
-  authDomain: "vue-demo-537e6.firebaseapp.com",
-  databaseURL: "https://vue-demo-537e6.firebaseio.com"
-}
+  apiKey: "AIzaSyDEfmzmbGAJikMm4ypkLcOS1REGonDvHAk",
+  authDomain: "vue-mile-2.firebaseapp.com",
+  databaseURL: "https://vue-mile-2.firebaseio.com",
+  }
 firebase.initializeApp(config)
 
 var usersRef = firebase.database().ref('users')
