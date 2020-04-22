79a6b0fef729da4e5987b46d68492352f2caaf07
mileallen <31962034+mileallen@users.noreply.github.com>
2020-04-22 17:49:19 +0530

    Hanzi version 1.9
    You can now pick the tiles. Some cosmetics.

--- hanzi/code.js
+++ hanzi/code.js
@@ -1,7 +1,8 @@
 
-function getCards() {
 
-    const kanjis = [
+function hanyuGame() {
+
+    var kanjis = [
         {id: 1, char:'的', flipped: false, matched: false},
         {id: 2, char:'在', flipped: false, matched: false},
         {id: 3, char:'有', flipped: false, matched: false},
@@ -10,7 +11,7 @@ function getCards() {
         {id: 6, char:'谢', flipped: false, matched: false},
     ];
    
-    const words = [
+    var words = [
         {id: 1, mean: 'de - of', flipped: false, matched: false},
         {id: 2, mean: 'Zài - in, at', flipped: false, matched: false},
         {id: 3, mean: 'Yǒu - have', flipped: false, matched: false},
@@ -18,35 +19,48 @@ function getCards() {
         {id: 5, mean: 'Qǐng - please', flipped: false, matched: false},
         {id: 6, mean: 'xie - thanks', flipped: false, matched: false},
     ];
-
-    return [...kanjis, ...words]
-    .sort(() => Math.random() - Math.random())
-
-
-}
-
-
-function hanyuGame () {
     
+
 return {
 
-    cards: getCards(),
+    kanjis,
+    words,
+
+    homePage: true,
+
+    cards: [...kanjis, ...words]
+    .sort(() => Math.random() - Math.random()),
 
     twoCards: { flip1: {}, flip2: {} },
 
     totalMatches: 0,
     countFlip: 0,
+    gameOver: false,
 
     resetGame(){
-        this.cards = getCards()
+
+        this.gameOver=false
+        this.homePage=true
+
+        kanjis.forEach(kanji => {
+            kanji.flipped=false
+            kanji.matched=false
+        })
+
+        words.forEach(word => {
+            word.flipped=false
+            word.matched=false
+        })
+
+        this.cards = [...kanjis, ...words]
+        .sort(() => Math.random() - Math.random())
         this.totalMatches=0
         this.countFlip=0
         this.twoCards.flip1 = this.twoCards.flip2 = null
     },
 
-    afterFlip(whichCard, cardDIV) {
+    afterFlip(whichCard) {
        
-    //console.log(whichCard)
     
     if (this.countFlip<1) { 
         this.countFlip++
@@ -61,12 +75,16 @@ return {
                 this.twoCards.flip1.matched=true
                 this.twoCards.flip2.matched=true
                 this.twoCards.flip1 = this.twoCards.flip2 = null
-                }, 500)
                 this.totalMatches++
+
+                if (this.totalMatches>5) {
+                    this.gameOver = true
+                    }
+
+                }, 450)
                 this.countFlip=0 
-            
-            //console.log("match!")      
-            }
+
+        }
                  
         else {                                 
             this.countFlip=0
@@ -74,10 +92,10 @@ return {
                 this.twoCards.flip1.flipped=false
                 this.twoCards.flip2.flipped=false
                 this.twoCards.flip1 = this.twoCards.flip2 = null
-                }, 1000)
-            }
+                }, 450)
+        }
 
-
+            
     }
 
 }
@@ -85,3 +103,6 @@ return {
 }
 
 }
+
+
+
